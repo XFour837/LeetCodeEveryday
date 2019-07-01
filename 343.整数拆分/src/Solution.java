7f24e2ae37eee2f4127a0e6cb6ed1c15a85623bb
XFour <837065279@qq.com>
2019-07-01 21:35:46 +0800

    LeetCodeEveryday

---
+++ 343.整数拆分/src/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/7/1
+ * Time: 21:16
+ * Description:
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ *
+ * 示例 1:
+ *
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ * 示例 2:
+ *
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ * 说明: 你可以假设 n 不小于 2 且不大于 58。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+
+    public int integerBreak(int n) {
+        //dp[i] 表示第 i 被拆分之后得到乘积的最大值
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
