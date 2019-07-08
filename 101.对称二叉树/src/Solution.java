612a05d7322311ad8875290280f9117b572def8c
XFour <837065279@qq.com>
2019-07-08 18:24:26 +0800

    LeetCodeEveryday

---
+++ 101.对称二叉树/src/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/7/8
+ * Time: 17:13
+ * Description:
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
+        if (node1.val != node2.val) return false;
+        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+    }
+}
