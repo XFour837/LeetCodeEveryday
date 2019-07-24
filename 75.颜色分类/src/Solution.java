edfd9c47eb85127302d143da5e91d9007c2df122
XFour <837065279@qq.com>
2019-07-24 23:43:58 +0800

    LeetCodeEveryday

---
+++ 75.颜色分类/src/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/7/24
+ * Time: 23:33
+ * Description:
+ */
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums.length == 0) return;
+        int z = -1;
+        int t = nums.length;
+        for (int i = 0; i < t;) {
+            if (nums[i] == 0) swap(nums, ++z, i++);
+            else if (nums[i] == 1) i++;
+            else swap(nums, --t, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
