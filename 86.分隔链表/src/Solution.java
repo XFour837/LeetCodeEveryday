2d6a7725681efe47137bea8ae30fad2e227fb06e
XFour <837065279@qq.com>
2019-08-14 00:56:09 +0800

    LeetCodeEveryday

---
+++ 86.分隔链表/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/8/14
+ * Time: 0:52
+ * Description:
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        return null;
+    }
+}
