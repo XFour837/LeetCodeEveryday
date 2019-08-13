93e338508d29d26ad39d05c562cd63b4b485d1e7
XFour <837065279@qq.com>
2019-08-13 22:49:39 +0800

    LeetCodeEveryday

---
+++ 447.回旋镖的数量/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/8/13
+ * Time: 22:31
+ * Description:
+ */
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        int count = 0;
+        for(int i = 0;i < points.length;i++){
+            hashMap.clear();
+            for(int j = 0;j < points.length;j++){
+                if(i == j){
+                    continue;
+                }
+                int distance = (points[i][0] - points[j][0])*(points[i][0] - points[j][0]) + (points[i][1] - points[j][1])*(points[i][1] - points[j][1]);
+                count += hashMap.getOrDefault(distance,0)*2;
+                hashMap.put(distance,hashMap.getOrDefault(distance,0) + 1);
+            }
+        }
+        return count;
+    }
+}
