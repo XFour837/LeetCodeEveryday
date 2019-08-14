f5ca6d046f8ebc9848466a7d223f6e4734b52fb8
XFour <837065279@qq.com>
2019-08-14 21:29:11 +0800

    LeetCodeEveryday

--- 86.分隔链表/src/Solution.java
+++ 86.分隔链表/src/Solution.java
@@ -14,6 +14,22 @@ class ListNode {
 
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        return null;
+        ListNode node1 = new ListNode(0);
+        ListNode node2 = new ListNode(0);
+        ListNode before = node1;
+        ListNode after = node2;
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+        after.next = null;
+        before.next = node2.next;
+        return node1.next;
     }
 }
