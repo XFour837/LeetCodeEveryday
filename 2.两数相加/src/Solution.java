f893d999e450b75bb8b770a9e8d5305984ea19d6
XFour <837065279@qq.com>
2019-08-14 21:59:40 +0800

    LeetCodeEveryday

--- 2.两数相加/src/Solution.java
+++ 2.两数相加/src/Solution.java
@@ -5,65 +5,35 @@
  * Time: 19:16
  * Description:注意 5 + 5这种情况
  */
-public class Solution {
 
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
 
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
-        ListNode node = addTwoNumbers(l1, l2);
-
-        System.out.println("test");
-    }
-
-    static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        if (l1 == null || l2 == null) {
-            throw new IllegalArgumentException("ListNode must not be null");
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        boolean addFlag = false;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
+            int sum = num1 + num2;
+            if (addFlag) sum++;
+            if (sum >= 10) {
+                addFlag = true;
+                sum = sum % 10;
+            } else {
+                addFlag = false;
+            }
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-
-        int num = 0;
-
-        ListNode res = new ListNode((l1.val + l2.val) % 10);
-        num = (l1.val + l2.val) / 10;
-        ListNode p = res;
-        l1 = l1.next;
-        l2 = l2.next;
-
-        while (true) {
-            if (l1 == null && l2 == null && num == 0) {
-                break;
-            }
-
-            if (l1 == null) {
-                l1 = new ListNode(0);
-            }
-
-            if (l2 == null) {
-                l2 = new ListNode(0);
-            }
-
-            p.next = new ListNode((l1.val + l2.val + num) % 10);
-            num = (l1.val + l2.val + num) / 10;
-
-            l1 = l1.next;
-            l2 = l2.next;
-
-            p = p.next;
-        }
-
-        return res;
+        if (addFlag) cur.next = new ListNode(1);
+        return dummy.next;
     }
-
 }
