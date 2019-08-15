2628b8b4f98a0ab3864ca00f8f237f38a4cf3c24
XFour <837065279@qq.com>
2019-08-16 00:44:30 +0800

    LeetCodeEveryday

---
+++ 103.二叉树的锯齿形层次遍历/src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/8/16
+ * Time: 0:39
+ * Description:
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        boolean left = true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            if (!left) Collections.reverse(list);
+            left = !left;
+            res.add(list);
+        }
+        return res;
+    }
+}
