304c0346c95fcc761ab5987f4dfc95bce885ef7b
XFour <837065279@qq.com>
2019-08-17 19:39:44 +0800

    LeetCodeEveryday

--- 39.组合总和/src/Solution.java
+++ 39.组合总和/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,7 +10,29 @@ import java.util.List;
  * Description:
  */
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
+    private List<List<Integer>> res = new ArrayList<>();
+    private int[] nums;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) return res;
+        Arrays.sort(candidates);
+        this.nums = candidates;
+        combinationSum(target, new ArrayList<>(), 0);
+        return res;
     }
+
+    private void combinationSum(int target, List<Integer> cur, int index) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            if (nums[i] > target) break;
+            cur.add(nums[i]);
+            combinationSum(target - nums[i], cur, i);
+            cur.remove(cur.size() - 1);
+        }
+    }
+
 }
