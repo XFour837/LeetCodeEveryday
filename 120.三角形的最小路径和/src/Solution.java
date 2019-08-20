96eabba07636d0c4d5a0201a2d427197d45189d9
XFour <837065279@qq.com>
2019-08-20 22:32:21 +0800

    LeetCodeEveryday

--- 120.三角形的最小路径和/src/Solution.java
+++ 120.三角形的最小路径和/src/Solution.java
@@ -29,17 +29,13 @@ import java.util.List;
 class Solution {
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        if (triangle == null || triangle.size() == 0) {
-            return 0;
-        }
-        for (int i = triangle.size() - 2; i >= 0; i--) {
+        for (int i = triangle.size() - 2; i >= 0 ; i--) {
             for (int j = 0; j < triangle.get(i).size(); j++) {
-                triangle.get(i).set(j, triangle.get(i).get(j)
-                        +Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+                triangle.get(i).set(j, Math.min(triangle.get(i + 1).get(j),
+                        triangle.get(i + 1).get(j + 1)) + triangle.get(i).get(j));
             }
         }
         return triangle.get(0).get(0);
     }
-
 
 }
---
+++ 64.最小路径和/src/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Create with LeetCode
+ * User: 许清远
+ * Date: 2019/8/20
+ * Time: 22:20
+ * Description:
+ */
+class Solution {
+    public int minPathSum(int[][] grid) {
+        for (int i = 1; i < grid[0].length; i++) {
+            grid[0][i] = grid[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (j == 0) {
+                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                } else {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
