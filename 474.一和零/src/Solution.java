12a2c7340a599ad232b3045552abc76cf391e261
XFour <837065279@qq.com>
2019-08-23 00:14:18 +0800

    LeetCodeEveryday

--- 474.一和零/src/Solution.java
+++ 474.一和零/src/Solution.java
@@ -8,16 +8,14 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
+        for (String str : strs) {
             char[] chars = str.toCharArray();
             int zero = 0, one = 0;
             for (char c : chars) {
                 if (c == '0') zero++;
                 else one++;
             }
-
-            for (int j = m; j >= zero ; j--) {
+            for (int j = m; j >= zero; j--) {
                 for (int k = n; k >= one; k--) {
                     dp[j][k] = Math.max(dp[j][k], dp[j - zero][k - one] + 1);
                 }
