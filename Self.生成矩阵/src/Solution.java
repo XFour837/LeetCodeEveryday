b45737db852aff1d4cc20230e49100d3de9ae85b
XFour <837065279@qq.com>
2019-11-22 16:12:28 +0800

    LeetCodeEveryday

--- Self.生成矩阵/src/Solution.java
+++ Self.生成矩阵/src/Solution.java
@@ -3,7 +3,44 @@
  * User: XFour
  * Date: 2019/11/22
  * Time: 15:57
- * Description:
+ * Description: 给一个n，生成一个n*n的矩阵，矩阵最外面一圈是1，里面一圈是2，再里面是3，以此类推。
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.generate(5);
+    }
+
+    public void generate(int N) {
+        N = 2 * N;
+        int flag = 0;
+        for (int i = 1; i <= N / 2; i++) {
+            for (int j = 1; j <= flag; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 0; j < N - 2 * flag; j++) {
+                System.out.print(i + " ");
+            }
+            for (int j = flag; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            flag++;
+            System.out.println();
+        }
+        for (int i = N / 2; i >= 1; i--) {
+            for (int j = 1; j <= flag; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 0; j < N - 2 * flag; j++) {
+                System.out.print(i + " ");
+            }
+            for (int j = flag; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            flag--;
+            System.out.println();
+        }
+    }
+
 }
